add8cd4dab4511e509c1598a03f68f2f93735595
n42k <n101010k@gmail.com>
2016-10-10 11:39:58 +0100

    Initial commit for the first report.

---
+++ reports/report0.mk
@@ -0,0 +1,10 @@
+# Possible topics to discuss
+
+## Current development
+* Continuous Integration
+* Agile
+
+## Earlier development
+* Reverse Engineering
+* Reversal of usual software development processes
+* Regression Testing
